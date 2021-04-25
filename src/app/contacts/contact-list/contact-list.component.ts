ecf909e185489355b54bc812371fb3107104f674
Cara <caradevey@yahoo.com>
2021-04-24 22:17:21 -0600

    Mostly done. Still needs style fixes.

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model'
 
 @Component({
   selector: 'app-contact-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact( 1, 'R. Kent Jackson', 'jacksonk@byui.edu', 208-496-3771, 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null),
+    new Contact(2, 'Rex Barzee', 'barzeer@byui.edu', 208-496-3768, 'https://web.byui.edu/Directory/Employee/barzeer.jpg', null)
+    
+  ];
 
   constructor() { }
 
