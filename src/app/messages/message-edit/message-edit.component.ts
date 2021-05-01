85536c540d8a188fc8f835b1cf7fd8d959b39ecd
Cara <caradevey@yahoo.com>
2021-04-30 18:27:11 -0600

    Week 3 assignment done!

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Message } from '../messages.model';
 
 @Component({
   selector: 'app-message-edit',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  @ViewChild('subject') subjectInputRef: ElementRef;
+  @ViewChild('msgText') msgTextInputRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender = 'Cara';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSendMessage() {
+    const subjectin = this.subjectInputRef.nativeElement.value;
+    const msgTextin = this.msgTextInputRef.nativeElement.value;
+    const newMessage = new Message(1, subjectin, msgTextin, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+
+  }
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgTextInputRef.nativeElement.value = '';
+  }
+
 }
+
+
+
+
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../messages.model';
 
 @Component({
   selector: 'app-message-list',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageListComponent implements OnInit {
 
+  messages: Message[] = [
+    new Message(1, 'hi','How are you?', 'Bob'),
+    new Message(2, 'Happy','Are you happy?', 'Tupper'),
+    new Message(3, 'Rush','Come now', 'Susan')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message){
+    this.messages.push(message);
+
   }
 
 }
