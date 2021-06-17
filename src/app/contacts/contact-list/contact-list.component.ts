f0dcfd4d03c171e6f777187879fe2744dfc3cdfc
Cara <caradevey@yahoo.com>
2021-06-16 20:21:42 -0600

    pipes and search working

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { Contact } from '../contact.model'
 import { ContactService } from '../contact.service';
 
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
@@ -13,6 +14,8 @@ export class ContactListComponent implements OnInit {
   //@Input() contact: Contact;
 
   contacts: Contact[];
+  term: string;
+
 
   constructor(private contactService: ContactService) { }
 
@@ -29,6 +32,12 @@ export class ContactListComponent implements OnInit {
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+
+  search(value: string) {
+
+    this.term = value;
+    
+    }
  
 
 }
