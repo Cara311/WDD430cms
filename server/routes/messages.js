d880f147ef079ca9eae86f04aefc2598d56db425
Cara <caradevey@yahoo.com>
2021-06-30 17:50:50 -0600

    Trying to Figure Out Assignment

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,3 +1,95 @@
 var express = require('express');
 var router = express.Router();
 module.exports = router; 
+
+const sequenceGenerator = require('./sequenceGenerator');
+const Message = require('../models/message');
+
+
+router.get("/", (req, res, next) => {
+    Message.find().then(messages => {
+        if (messages) {
+            res.status(200).json(messages);
+          } else {
+            res.status(500).json({ message: "Post not found!", error: error });
+          }
+    });
+  });
+
+ router.post('/', (req, res, next) => {
+    const maxMessageId = sequenceGenerator.nextId("messages");
+  
+    const message = new Message({
+      id: maxMessageId,
+      name: req.body.name,
+      description: req.body.description,
+      url: req.body.url
+    });
+  
+    message.save()
+      .then(createdMessage => {
+        res.status(201).json({
+          message: 'Message added successfully',
+          document: createdMessage
+        });
+      })
+      .catch(error => {
+         res.status(500).json({
+            message: 'An error occurred',
+            error: error
+          });
+      });
+  });
+
+
+router.put('/:id', (req, res, next) => {
+    Message.findOne({ id: req.params.id })
+      .then(document => {
+        document.name = req.body.name;
+        document.description = req.body.description;
+        document.url = req.body.url;
+  
+        Message.updateOne({ id: req.params.id }, document)
+          .then(result => {
+            res.status(204).json({
+              message: 'Document updated successfully'
+            })
+          })
+          .catch(error => {
+             res.status(500).json({
+             message: 'An error occurred',
+             error: error
+           });
+          });
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: 'Document not found.',
+          error: { document: 'Document not found'}
+        });
+      });
+  });
+
+  router.delete("/:id", (req, res, next) => {
+    Document.findOne({ id: req.params.id })
+      .then(document => {
+        Document.deleteOne({ id: req.params.id })
+          .then(result => {
+            res.status(204).json({
+              message: "Document deleted successfully"
+            });
+          })
+          .catch(error => {
+             res.status(500).json({
+             message: 'An error occurred',
+             error: error
+           });
+          })
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: 'Document not found.',
+          error: { document: 'Document not found'}
+        });
+      });
+  });
